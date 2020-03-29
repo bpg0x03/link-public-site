950e3d58c8c5e2547616dae0da76ab76f2ea2ef9
Tripp Gallagher <tripp.gallagher@protonmail.com>
2020-03-29 03:14:57 -0400

    debug

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var app, server
 express = require('express')
 path = require('path')
-host = process.env.HOST || '127.0.0.1'
 port = process.env.PORT || 8080
 root = path.resolve(__dirname, '.')
 
@@ -9,7 +8,7 @@ app = express()
 app.use(express.static(root+'/jekyll/_site/'))
 
 
-server = app.listen(port, host, ()=>{
-	console.log('server started')
+server = app.listen(port, ()=>{
+	console.log('server started ' + port)
 })
 
